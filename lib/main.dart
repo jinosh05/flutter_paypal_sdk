6c01df48ffa1ff4613d4cc7a819aa890b58431e9
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-09-11 14:31:30 +0530

    Packages added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_paypal_sdk/src/screens/network_error.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
