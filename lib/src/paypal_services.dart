39a5e97d04cfcf1d9c7627d7090f97b08102dfbc
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-09-11 15:58:17 +0530

    createPaypalPayment method created

--- lib/src/paypal_services.dart
+++ lib/src/paypal_services.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter_paypal_sdk/src/constants/constants.dart';
 import 'package:flutter_paypal_sdk/src/constants/error_jsons.dart';
+import 'package:http/http.dart';
 import 'package:http_auth/http_auth.dart';
 
 class PaypalServices {
@@ -13,7 +14,7 @@ class PaypalServices {
     required this.sandboxMode,
   });
 
-  getAccessToken() async {
+  Future<Map> getAccessToken() async {
     String domain = sandboxMode ? Constants.sandboxUrl : Constants.directUrl;
     try {
       var client = BasicAuthClient(clientId, secretKey);
@@ -31,6 +32,48 @@ class PaypalServices {
       }
     } catch (e) {
       return ErrorJsons.noInternetError;
+    }
+  }
+
+  Future<Map> createPaypalPayment(transactions, String accessToken) async {
+    String domain = sandboxMode ? Constants.sandboxUrl : Constants.directUrl;
+    try {
+      var header = {
+        "content-type": "application/json",
+        'Authorization': 'Bearer $accessToken'
+      };
+      var response = await post(
+        Uri.parse("$domain${Constants.createPaymentBase}"),
+        body: jsonEncode(transactions),
+        headers: header,
+      );
+
+      final body = jsonDecode(response.body);
+
+      if (response.statusCode == 201) {
+        if (body["links"] != null && body["links"].length > 0) {
+          List links = body["links"];
+
+          String executeUrl = "";
+          String approvalUrl = "";
+          final item = links.firstWhere((o) => o["rel"] == "approval_url",
+              orElse: () => null);
+          if (item != null) {
+            approvalUrl = item["href"];
+          }
+          final item1 = links.firstWhere((o) => o["rel"] == "execute",
+              orElse: () => null);
+          if (item1 != null) {
+            executeUrl = item1["href"];
+          }
+          return {"executeUrl": executeUrl, "approvalUrl": approvalUrl};
+        }
+        return {};
+      } else {
+        return body;
+      }
+    } catch (e) {
+      rethrow;
     }
   }
 }
