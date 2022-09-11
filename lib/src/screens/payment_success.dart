0066638168f627e3be655ec22dd8c56f8724f08a
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-09-11 17:05:59 +0530

    complete payment method done in Payment Screen

--- lib/src/screens/payment_success.dart
+++ lib/src/screens/payment_success.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../constants/sizes.dart';
+import '../paypal_services.dart';
 import 'network_error.dart';
 
 class PaymentSuccess extends StatefulWidget {
   final Function onSuccess, onCancel, onError;
-  // final PaypalServices services;
+  final PaypalServices services;
   final String url, executeUrl, accessToken;
-  const PaymentSuccess(
-      {super.key,
-      required this.onSuccess,
-      required this.onCancel,
-      required this.onError,
-      required this.url,
-      required this.executeUrl,
-      required this.accessToken});
+  const PaymentSuccess({
+    super.key,
+    required this.onSuccess,
+    required this.onCancel,
+    required this.onError,
+    required this.url,
+    required this.executeUrl,
+    required this.accessToken,
+    required this.services,
+  });
 
   @override
   State<PaymentSuccess> createState() => _PaymentSuccessState();
@@ -30,7 +33,52 @@ class _PaymentSuccessState extends State<PaymentSuccess> {
     complete();
   }
 
-  complete() async {}
+  complete() async {
+    final uri = Uri.parse(widget.url);
+    final payerId = uri.queryParameters['PayerID'];
+    if (payerId != null) {
+      Map params = {
+        "payerID": payerId,
+        "paymentId": uri.queryParameters['paymentId'],
+        "token": uri.queryParameters['token'],
+      };
+      setState(() {
+        loading = true;
+        loadingError = false;
+      });
+
+      Map resp = await widget.services
+          .executePayment(widget.executeUrl, payerId, widget.accessToken);
+      if (resp['error'] == false) {
+        params['status'] = 'success';
+        params['data'] = resp['data'];
+        await widget.onSuccess(params);
+        setState(() {
+          loading = false;
+          loadingError = false;
+        });
+        _popScreen();
+      } else {
+        if (resp['exception'] != null && resp['exception'] == true) {
+          widget.onError({"message": resp['message']});
+          setState(() {
+            loading = false;
+            loadingError = true;
+          });
+        } else {
+          await widget.onError(resp['data']);
+          _popScreen();
+        }
+      }
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
+  _popScreen() {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     Sizes s = Sizes(context);
